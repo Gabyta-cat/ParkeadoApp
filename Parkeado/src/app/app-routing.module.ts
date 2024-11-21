a6e1ddae6c26b54ab00f550ebed0e66bd656e9b6
Gabyta-cat <ga.barraza@duocuc.cl>
2024-11-20 21:42:49 -0300

    Creando page y component

--- Parkeado/src/app/app-routing.module.ts
+++ Parkeado/src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./Paginas/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Paginas/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperar',
+    loadChildren: () => import('./Paginas/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./Paginas/registro/registro.module').then( m => m.RegistroPageModule)
   },
 ];
 
