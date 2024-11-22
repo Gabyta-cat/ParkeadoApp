9db9e4300371777b3ef046dd2d70780e6527e996
Gabyta-cat <ga.barraza@duocuc.cl>
2024-11-22 17:01:28 -0300

    cambiando

--- Parkeado/src/app/Paginas/home/home.page.ts
+++ Parkeado/src/app/Paginas/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular'; 
+import { ActivatedRoute } from '@angular/router'; //Librería para recibir parámetros de la URL
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  receivedUsername!: string; // Recibe el nombre de usuario
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private alertController: AlertController, private navCtrl: NavController) { }
 
   ngOnInit() {
+    // Obtener el parámetro del nombre de usuario desde la URL
+    this.route.queryParams.subscribe(params => {
+      this.receivedUsername = params['user']; // Almacena lo recibido desde Login
+    });
   }
 
 }
