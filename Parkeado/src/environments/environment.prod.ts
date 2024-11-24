5a8bc8976357cf6126e44d270ab340b6a581ad0e
Gabyta-cat <ga.barraza@duocuc.cl>
2024-11-24 02:10:35 -0300

    Firebase

--- Parkeado/src/environments/environment.prod.ts
+++ Parkeado/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyA5ZhMRP_s4JYXVS6FhcmSmWzkD5t1vLs0",
+    authDomain: "parkeado-app-2bf0f.firebaseapp.com",
+    projectId: "parkeado-app-2bf0f",
+    storageBucket: "parkeado-app-2bf0f.firebasestorage.app",
+    messagingSenderId: "646919569979",
+    appId: "1:646919569979:web:b006a6f8c85aaaa025d21e",
+    measurementId: "G-PNCQ0JTZKN"
+  }
 };
--- Parkeado/src/environments/environment.ts
+++ Parkeado/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyA5ZhMRP_s4JYXVS6FhcmSmWzkD5t1vLs0",
+    authDomain: "parkeado-app-2bf0f.firebaseapp.com",
+    projectId: "parkeado-app-2bf0f",
+    storageBucket: "parkeado-app-2bf0f.firebasestorage.app",
+    messagingSenderId: "646919569979",
+    appId: "1:646919569979:web:b006a6f8c85aaaa025d21e",
+    measurementId: "G-PNCQ0JTZKN"
+  }
 };
 
 /*
