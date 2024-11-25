1e5c087d24032c48e37e4b0b5242901cc3cf4d5e
Gabyta-cat <ga.barraza@duocuc.cl>
2024-11-25 01:42:50 -0300

    Agregando estacionamentos boton

--- Parkeado/src/app/shared/components/add-update/add-update.component.ts
+++ Parkeado/src/app/shared/components/add-update/add-update.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.module';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-add-update',
@@ -6,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-update.component.scss'],
 })
 export class AddUpdateComponent  implements OnInit {
+  form = new FormGroup({
+    id: new FormControl(''),
+    image: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    price: new FormControl('', [Validators.required, Validators.min(0)]),
+    soldUnits: new FormControl('', [Validators.required, Validators.min(0)])
+  })
 
-  constructor() { }
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService)
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
+  async submit() {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      this.firebaseSvc.signUp(this.form.value as User).then(async res => {
+
+        await this.firebaseSvc.updateUser(this.form.value.name);
+
+        let uid = res.user.uid;
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
+  }
 }
+
