69a37fe93b537d1a340914bd3deb76ac921567da
Gabyta-cat <ga.barraza@duocuc.cl>
2024-11-24 23:39:40 -0300

    splash

--- Parkeado/src/app/pages/splash/splash.page.ts
+++ Parkeado/src/app/pages/splash/splash.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'; //importar el modulo Router
 
 @Component({
   selector: 'app-splash',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash.page.scss'],
 })
 export class SplashPage implements OnInit {
-
-  constructor() { }
+//Modificar el constructor o función para iniciar la page login
+// cuando finalice la animación de la page splash
+  constructor(public router: Router) {
+    setTimeout(() => {
+      this.router.navigateByUrl('auth')
+   }, 2000);//duración 3 segundos
+  }
 
   ngOnInit() {
   }
-
 }
