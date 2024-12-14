a4df278bcb6f60a2dbea8f11de945096120df2e2
Gabyta-cat <ga.barraza@duocuc.cl>
2024-12-14 02:21:56 -0300

    android

--- Parkeado/capacitor.config.ts
+++ Parkeado/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.parkeado.app',
-  appName: 'com.parkeado.app',
+  appName: 'Parkeado',
   webDir: 'www'
 };
 
